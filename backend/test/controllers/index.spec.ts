1f1ad1339438c73c0a6f63bea7a8b0cae9dec1b6
Hasi6 <hasitha.chandula@gmail.com>
2023-09-10 10:18:06 +0530

    [refactor]: move test cases to one file

--- backend/test/controllers/index.spec.ts
+++ backend/test/controllers/index.spec.ts
@@ -1,16 +1,11 @@
 import request from 'supertest';
 
-import app, { server } from '../../../src';
-import { sleep } from '../../../src/utils/helpers';
+import app from '../../src';
 
 let authorId = '';
+let bookId = '';
 
 describe('Author Endpoints', () => {
-  afterAll(async () => {
-    server.close();
-    await sleep(500);
-  });
-
   it('should return all authors', async () => {
     const res = await request(app).get('/api/v1/authors');
     expect(res.statusCode).toEqual(200);
@@ -43,5 +38,48 @@ describe('Author Endpoints', () => {
     expect(res.statusCode).toEqual(200);
     expect(res.body.data.firstName).toEqual(firstName);
     expect(res.body.data.lastName).toEqual(lastName);
+  });
+});
+
+describe('Book Endpoints', () => {
+  it('should return all books', async () => {
+    const res = await request(app).get('/api/v1/books');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should get 400 when request body is empty when trying to create a book', async () => {
+    const res = await request(app).post('/api/v1/books');
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('should get 400 when request body is empty when trying to update a book', async () => {
+    const res = await request(app).put('/api/v1/books/12');
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it('should create new book', async () => {
+    const res = await request(app)
+      .post('/api/v1/books')
+      .send({
+        name: `New Book ${Date.now()}`,
+        isbn: `New Book ${Date.now()}`,
+        authorId
+      });
+    expect(res.statusCode).toEqual(201);
+    bookId = res.body.data.id;
+    expect(bookId).toBeDefined();
+  });
+
+  it('should update the book', async () => {
+    const name = `Updated book name ${Date.now()}`;
+    const isbn = `Updated isbn ${Date.now()}`;
+    const res = await request(app).put(`/api/v1/books/${bookId}`).send({
+      name,
+      isbn,
+      authorId
+    });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.data.name).toEqual(name);
+    expect(res.body.data.isbn).toEqual(isbn);
   });
 });
